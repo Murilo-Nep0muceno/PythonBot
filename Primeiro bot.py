9d81519db5b7925bf08918379024dc37dd6c41e2
Murilo Nepomuceno <murilonep@gmail.com>
2022-04-12 20:33:28 -0300

    commit inicial

--- Primeiro bot.py
+++ Primeiro bot.py
@@ -37,18 +37,18 @@ async def calculate(ctx, *expression):
     print(expression)
     responce = eval(expression)
 
-    await ctx.send("a resposta é: vai se fuder,mais facil usar calculadora " + name)
-    await ctx.send("toma aqui sua resposta: " + str(responce))
+    await ctx.send("a resposta é.... Não vou falar " + name)
+    await ctx.send(" brincadeira toma aqui sua resposta: " + str(responce))
 
 
 
 
-@bot.command(name="odio")
+@bot.command(name="amor")
 async def segredo(ctx):
      name = ctx.author.name
-     await ctx.send("=====Declaração de odio ===== ")
-     await ctx.send("Você merece o lixo e não o luxo " + name )
-     await ctx.send("nem se você tira seu cerebro e colocar um novo voce fica boa em um jogo " + name)
+     await ctx.send("=====Declaração de amor===== ")
+     await ctx.send("Você é lindo(a) você merece o mundo " + name )
+     await ctx.send("não se preocupe, isso é so uma fase vai passar " + name)
 
 
 
